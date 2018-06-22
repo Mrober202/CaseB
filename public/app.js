ae0b008c5249ee2ccbc848937b08923bfa78e58f
Michael Robertson <michael_robertson_92@hotmail.co.uk>
2018-06-22 18:15:11 +0100

    added function to update API after user input

--- public/app.js
+++ public/app.js
@@ -7,3 +7,27 @@ var enquiryBuilder = function(userInput) {
   var url = "https://testk.caseblocks.com/case_blocks/search.json?query=customer_ref:" + userInput = "&auth_token=Rgik46EHz-C7sx5xonCV";
   return url;
 }
+
+var makeRequest = function(url, callback) {
+  var request = XMLHttpRequest();
+  request.open();
+  request.addEventListener("load", callback);
+  request.send();
+}
+
+var requestComplete = function() {
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  var apiResponse = JSON.parse(jsonString);
+  var info = apiResponse[0];
+  showResult(apiResponse);
+}
+
+var submitButtonHandler = function() {
+  var enteredInput = document.getElementById("enter-info");
+  var input = enteredInput.value;
+  var url = enquiryBuilder(input);
+  var result = makeRequest(url, requestComplete);
+}
+
+
